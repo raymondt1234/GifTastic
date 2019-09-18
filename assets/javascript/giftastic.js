918a5f4164dc6e11b1b838e1731ec5d582999398
Raymond _ <raymondt1234@gmail.com>
2019-09-18 11:38:51 -0700

    The buttons now put up images when pressed

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,23 +1,32 @@
 function addButtons() {
     topics.forEach(function (topic) {
-        $("#topicBtns").append(`<button type="button" class="btn btn-info btnMargin" id=${topic}>${topic}</button>`);
+        let topicId = topic.replace(" ", "+");
+        $("#topicBtns").append(`<button type="button" class="btn btn-info btnMargin" id=${topicId}>${topic}</button>`);
     });
 }
-let topics = ["astartes", "cats", "avengers", "infinity-war", "final-fantasy"];
+let topics = ["astartes", "cats", "avengers", "infinity war", "final fantasy"];
 
 let apiKey = "135FLYSdRS8ORkm0JLtV4609cBrkSIYb";
 addButtons();
 
-$.ajax({
-    url: `http://api.giphy.com/v1/gifs/search?q=${topics[0]}&api_key=${apiKey}&limit=10`,
-    method: "GET"
-}).then(
-    function (data) {$("#gifs").append(`<img src=${data[0].url}>`);}
-    //function (data) { console.log("success got data", data); }
-);
+
 
 $(document).ready(function () {
     $(document).on("click", ".btn", function () {
-        console.log($(this).attr("id"));
+        //console.log($(this).attr("id"));
+        
+        $("#gifs").empty();
+
+        let query = $(this).attr("id");
+        $.ajax({
+            url: `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&limit=10`,
+            method: "GET"
+        }).then(
+            function (response) {
+                for (let i = 0; i < 10; i++) {
+                    $("#gifs").append(`<img src="${response.data[i].images.downsized.url}">`);
+                }
+            }
+        );
     });
 });
