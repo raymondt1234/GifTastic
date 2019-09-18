682f41834c62084d4176e1f86ee0505fd6dca93f
raymondt1234 <raymondt1234@gmail.com>
2019-09-17 20:36:39 -0700

    fixed bug that was causing style.css not to link properly

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,11 +1,23 @@
-function addButtons () {
+function addButtons() {
     topics.forEach(function (topic) {
-        $("#topicBtns").append(`<button type="button" class="btn btn-info" id=${topic}>${topic}</button>`);
+        $("#topicBtns").append(`<button type="button" class="btn btn-info btnMargin" id=${topic}>${topic}</button>`);
     });
 }
-let topics = ["astartes","cats","avengers", "infinity-war", "final-fantasy"];
+let topics = ["astartes", "cats", "avengers", "infinity-war", "final-fantasy"];
+
+let apiKey = "135FLYSdRS8ORkm0JLtV4609cBrkSIYb";
 addButtons();
 
-$(".btn").on("click", function(){
-    console.log($(this).attr("id"));    
+$.ajax({
+    url: `http://api.giphy.com/v1/gifs/search?q=${topics[0]}&api_key=${apiKey}&limit=10`,
+    method: "GET"
+}).then(
+    function (data) {$("#gifs").append(`<img src=${data[0].url}>`);}
+    //function (data) { console.log("success got data", data); }
+);
+
+$(document).ready(function () {
+    $(document).on("click", ".btn", function () {
+        console.log($(this).attr("id"));
+    });
 });
