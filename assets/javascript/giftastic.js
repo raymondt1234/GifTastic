001e5b49d07da43cd5a3901d9e21aaaacf841951
raymondt1234 <raymondt1234@gmail.com>
2020-06-27 09:52:23 -0700

    Switched up how images are added with jQuery

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -18,13 +18,30 @@ $(document).ready(function () {
 
         let query = $(this).attr("id");
         $.ajax({
-            url: `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&limit=10`,
+            url: `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&limit=9`,
             method: "GET"
         }).then(
             function (response) {
                 giphyObject = response;
                 for (let i = 0; i < 10; i++) {
-                    $("#gifs").append(`<div class="img-div"><p>Rating: ${giphyObject.data[i].rating}</p><img src="${giphyObject.data[i].images.downsized_still.url}" value="gif_still" id="${i}"></div>`);
+                    
+                    let rating = giphyObject.data[i].rating;
+                    let stillImage = giphyObject.data[i].images.downsized_still.url;
+                    
+                    let pTag = $("<p>").text(`Rating:${rating}`);
+                    let image = $("<img>");
+                    let imgDiv = $("<div>");
+
+                    imgDiv.attr("class", "col-md-4 img-div img-fluid");
+
+                    image.attr("src", stillImage);
+                    image.attr("value", "gif_still");
+                    image.attr("id", i);
+
+                    imgDiv.append(image);
+                    imgDiv.append(pTag);
+
+                    $("#gifs").append(imgDiv);
                 }
             }
         );
@@ -33,13 +50,9 @@ $(document).ready(function () {
         let i = Number($(this).attr("id"));
 
         if ($(this).attr("value") === "gif_still") {
-            //console.log(`${giphyObject.data[i].images.downsized.url}`);
-
             $(this).attr("src", `${giphyObject.data[i].images.downsized.url}`);
             $(this).attr("value", "gif_notStill");
         } else {
-            //console.log(`${giphyObject.data[i].images.downsized_still.url}`);
-
             $(this).attr("src", `${giphyObject.data[i].images.downsized_still.url}`);
             $(this).attr("value", "gif_still");
         }
@@ -54,7 +67,7 @@ $(document).ready(function () {
         if (!topic) {
             return;
         }
-        
+
         topics.push(topic);
         addButtons();
 
